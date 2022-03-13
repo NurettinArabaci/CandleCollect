7a744b042b33a74d795f65a0c3860149d6ed9105
NurettinArabaci <nurettinarabaci@gmail.com>
2022-03-13 12:14:16 +0300

    Growth or decrease
    obstacle ya da collactable objelere çarptıkça büyüme ya da küçülme kodları yazıldı.

--- Nurettin-CandleCollect/Assets/Scripts/ColliderController.cs
+++ Nurettin-CandleCollect/Assets/Scripts/ColliderController.cs
@@ -5,18 +5,33 @@ using UnityEngine;
 public class ColliderController : MonoBehaviour
 {
 
-    int scoreAmount = 1;
+    public static int scoreAmount = 1;
+
+
     private void OnTriggerEnter(Collider other)
     {
         if (other.tag=="Obstacle")
         {
             scoreAmount--;
             Debug.Log(scoreAmount);
+            TakeDamage();
         }
         else if (other.tag == "Collactable")
         {
             scoreAmount++;
             Debug.Log(scoreAmount);
+            CollectCandle();
         }
+    }
+
+
+    public void TakeDamage()
+    {
+        transform.GetChild(scoreAmount).gameObject.SetActive(false);
+    }
+
+    public void CollectCandle()
+    {
+        transform.GetChild(scoreAmount-1).gameObject.SetActive(true);
     }
 }
