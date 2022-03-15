fbf070d0c13dd65c2690987328983ae172b769f5
NurettinArabaci <nurettinarabaci@gmail.com>
2022-03-15 15:10:04 +0300

    Add Cinemachine
    Cinemachine ile oyun sonuna kamera geçişi eklendi.

--- Nurettin-CandleCollect/Assets/Scripts/CameraController.cs
+++ Nurettin-CandleCollect/Assets/Scripts/CameraController.cs
@@ -5,8 +5,11 @@ using UnityEngine;
 public class CameraController : MonoBehaviour
 {
     [SerializeField] Transform player;
+   
+    
     private void LateUpdate()
     {
+        
         Vector3 pos = new Vector3(player.position.x * 0.2f, player.position.y + 7.5f, player.position.z - 15);
         transform.position = pos;
     }
--- Nurettin-CandleCollect/Assets/Scripts/GameEnding.cs
+++ Nurettin-CandleCollect/Assets/Scripts/GameEnding.cs
@@ -1,9 +1,12 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Cinemachine;
 
 public class GameEnding : MonoBehaviour
 {
+    public CinemachineVirtualCameraBase vCamOne;
+    public CinemachineVirtualCameraBase vCamTwo;
 
     private void OnTriggerEnter(Collider other)
     {
@@ -23,7 +26,8 @@ public class GameEnding : MonoBehaviour
     {
         transform.GetComponent<Animator>().enabled = true;
         transform.GetComponent<Animator>().SetBool("finish", true);
-
+        vCamOne.GetComponent<CameraController>().enabled = false;
+        vCamTwo.m_Priority = 11;
         Debug.Log("Level Completed");
     }
 }
